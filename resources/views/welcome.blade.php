a54fcc0f01fb80c9c435cec00c04b575c4f009b8
fiorelacayo <fiorelacayo@innovahora.com>
2025-03-21 18:16:34 -0500

    Primer commit - Subiendo proyecto inicial

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.template')
+
+@section('title', 'Página de Inicio')  <!-- Cambiar título dinámico -->
+
+@section('content')
+    <h2>Esta es la página de bienvenida</h2>
+    <p>Aquí puedes agregar el contenido principal que desees.</p>
+@endsection
